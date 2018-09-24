7fc583eedd0a2e665b9935f3d0bf6ffee4920ef3
Samanimkr <Samanimkr@Outlook.com>
2018-09-25 00:01:22 +0100

    Setup the home page and added a favicon

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
   render() {
+    var today = new Date();
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="wrapper">
+          <h1>Meet</h1>
+          <button id="facebookBtn">Login with Facebook</button>
+        </div>
+        <footer>Copyright &copy; {today.getFullYear()} Sessy Davtar.</footer>
       </div>
     );
   }
