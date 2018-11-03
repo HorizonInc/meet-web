34d2bf101e9b9000ac4d6a9a7c9e4ada34528a21
Samanimkr <samanimkr@outlook.com>
2018-11-03 19:33:51 +0000

    Configures ESLint for the Whole meet project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import Login from './scenes/Login';
-import Groups from './scenes/Groups';
+// import Login from './scenes/Login';
+import Groups from "./scenes/Groups";
 
-class App extends Component {
-    render() {
-        return (
-            <div style={styles.container}>
-                {/* <Login /> */}
-                <Groups />
-            </div>);
-    }
+class App extends React.Component {
+	render() {
+		return (
+			<div style={styles.container}>
+				{/* <Login /> */}
+				<Groups />
+			</div>);
+	}
 }
 
 export default App;
 
 const styles = {
-    container: {
-        height: '100%'
-    }
-}
+	container: {
+		height: "100%"
+	}
+};
