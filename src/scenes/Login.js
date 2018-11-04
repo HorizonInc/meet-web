7da8bf7fbd5a7897e797c4621ee9a4998c56ddbf
Samanimkr <samanimkr@outlook.com>
2018-11-04 20:59:01 +0000

    Adds the babel-eslint as a parser and makes a call to the server for the fb login route

--- src/scenes/Login.js
+++ src/scenes/Login.js
@@ -1,10 +1,23 @@
 import React from "react";
-import MaterialButton from "../components/MaterialButton";
+import axios from "axios";
 
+import MaterialButton from "../components/MaterialButton";
 import { styles } from "./styles/LoginStyles";
 import logo from "../images/meet_logo.png";
 
 export default class Login extends React.Component {
+	onFacebookLogin(){
+		axios
+			.get("http://192.168.0.115:8080/fb")
+			.then(response => {
+				console.log("Response: ", response);
+			})
+			.catch(error => {
+				console.log("Error: ", error);
+			});
+
+	}
+
 	render(){
 		var today = new Date();
 		return (
@@ -12,7 +25,10 @@ export default class Login extends React.Component {
 				<div style={styles.contentWrapper}>
 					<img src={logo} style={styles.logo} alt=''/>
 					<h1 style={styles.logoText}>meet</h1>
-					<MaterialButton style={styles.facebookBtn}><i style={{float:"left", margin: "3px 0 0 20px"}} className="fab fa-facebook-f"></i> Login with Facebook</MaterialButton>
+					<MaterialButton style={styles.facebookBtn} onClick={() => this.onFacebookLogin()}>
+						<i style={{float:"left", margin: "3px 0 0 20px"}} className="fab fa-facebook-f"></i>
+						Login with Facebook
+					</MaterialButton>
 				</div>
 				<footer style={styles.footer}>Copyright &copy; { today.getFullYear()} Horizon.</footer>
 			</div>
